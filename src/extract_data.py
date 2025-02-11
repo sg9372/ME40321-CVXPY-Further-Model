ad00273f537ec05973b539fe7d6b053ba5054606
sg9372 <143601864+sg9372@users.noreply.github.com>
2025-02-11 17:22:37 +0000

    Extract Data Working
    Extracts average values and mean in a given range

--- src/extract_data.py
+++ src/extract_data.py
@@ -1,4 +1,23 @@
 import pandas as pd
 
-def extract_data(file):
-    print("HELLO")
+def get_average_values(df, start_day, end_day):
+    # Filter based on the day range
+    filtered_df = df[(df['Day'] >= start_day) & (df['Day'] <= end_day)]
+
+    value_columns = [col for col in df.columns if col.startswith('Value')]
+    return filtered_df[value_columns].mean().to_numpy()
+
+def get_average_emissions(df, start_day, end_day):
+    # Filter based on the day range
+    filtered_df = df[(df['Day'] >= start_day) & (df['Day'] <= end_day)]
+
+    emission_columns = [col for col in df.columns if col.startswith('Emission')]
+    return filtered_df[emission_columns].mean().to_numpy()
+
+def extract_data(file, start, end):
+    # Read data
+    df = pd.read_excel(file, index_col = 1)    
+    
+    values = get_average_values(df, start, end)
+    emissions = get_average_emissions(df, start, end)
+    return [values, emissions]
--- src/main.py
+++ src/main.py
@@ -1,27 +1,27 @@
 from extract_data import extract_data 
 
-def main(file, n):
+def main(file, start, end, n):
     """
     Description
     """
-    data = extract_data(file)
+    [values, emissions] = extract_data(file, start, end)
     
     #if n==1:
         
-
-
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) != 2:                                  
-        print("Wrong ammount of input arguments, example usage:")   # Reject if >1 input argument.
-        print("'python src/main.py sample_data 1'")
+    if len(sys.argv) != 5:                                  
+        print("Wrong ammount of input arguments, example usage:")
+        print("'python src/main.py sample_data.xlsx 2 5 1'")
         sys.exit(1)
     
     try:
         file = str(sys.argv[1])
-        n = int(sys.argv[2])
+        start = int(sys.argv[2])
+        end = int(sys.argv[3])
+        n = int(sys.argv[4])
         if 1<=n<=2:
-            main(n)
+            main(file, start, end, n)
         else:
             print("Please enter an integer from 1 to 2 inclusive.")    
     except:
