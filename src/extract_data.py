803970970400b3765db1bf3cd39f34e5eb3ddf63
sg9372 <143601864+sg9372@users.noreply.github.com>
2025-02-12 10:20:06 +0000

    Tests ran for extract_data
    Module functional on sample test data set.

--- src/extract_data.py
+++ src/extract_data.py
@@ -1,22 +1,27 @@
 import pandas as pd
+import numpy as np
 
 def get_average_values(df, start_day, end_day):
     # Filter based on the day range
     filtered_df = df[(df['Day'] >= start_day) & (df['Day'] <= end_day)]
-
+    
+    # Gather data, find mean, and round to 2dp
     value_columns = [col for col in df.columns if col.startswith('Value')]
-    return filtered_df[value_columns].mean().to_numpy()
+    average = filtered_df[value_columns].mean().to_numpy()
+    return np.round(average, 2)
 
 def get_average_emissions(df, start_day, end_day):
     # Filter based on the day range
     filtered_df = df[(df['Day'] >= start_day) & (df['Day'] <= end_day)]
 
+    # Gather data, find mean, and round to 2dp
     emission_columns = [col for col in df.columns if col.startswith('Emission')]
-    return filtered_df[emission_columns].mean().to_numpy()
+    average = filtered_df[emission_columns].mean().to_numpy()
+    return np.round(average, 2)
 
 def extract_data(file, start, end):
     # Read data
-    df = pd.read_excel(file, index_col = 1)    
+    df = pd.read_excel(file)    
     
     values = get_average_values(df, start, end)
     emissions = get_average_emissions(df, start, end)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from extract_data import extract_data 
+from extract_data import extract_data
 
-def main(file, start, end, n):
+def main(file, start, mid, end):
     """
     Can alter holdings in each stock either by total allowable move in weighting or based on a maximum 
     ammount each holding can be altered by.
@@ -10,10 +10,10 @@ def main(file, start, end, n):
         2 individualChange
     """
     # Get average values in sampling region
-    [values, emissions] = extract_data(file, start, end)
+    [values, emissions] = extract_data(file, start, mid)
     
     # Calculate optimal weights
-    if 
+    
 
     #if n==1:
         
@@ -27,12 +27,13 @@ if __name__ == "__main__":
     try:
         file = str(sys.argv[1])
         start = int(sys.argv[2])
-        end = int(sys.argv[3])
-        n = int(sys.argv[4])
-        if 1<=n<=2:
-            main(file, start, end, n)
+        mid = int(sys.argv[3])
+        end = int(sys.argv[4])
+        method = int(sys.argv[5])
+        if method == 'total' or method == 'individual':
+            main(file, start, mid, end, method)
         else:
-            print("Please enter an integer from 1 to 2 inclusive.")    
+            print("Please enter a valid method e.g. 'total'.")    
     except:
         print("Wrong data type.")
         print("Please enter an integer from 1 to 2 inclusive.")
