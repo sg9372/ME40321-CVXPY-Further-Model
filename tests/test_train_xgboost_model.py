26ce38907bd02d2becc7c4041542aa1f4db0d313
sg9372 <143601864+sg9372@users.noreply.github.com>
2025-03-06 16:21:51 +0000

    Update test_train_xgboost_model.py

--- tests/test_train_xgboost_model.py
+++ tests/test_train_xgboost_model.py
@@ -40,13 +40,16 @@ class TestTrainXGBoostModel(unittest.TestCase):
         # Test if the model predictions are close to the average future values
         sheet_names, dates = get_sheet_names("Monthly FTSE Data - New.xlsx")
         model = None
-        n=40
-        for i in range(n):
+        n=20
+        for i in range(n): 
             sheet_name = sheet_names[i]
             end_date = dates[i+1]
             companies, _, _ = extract_sheet_data("Monthly FTSE Data - New.xlsx", sheet_name)
-            curr_values, fut_values,_ = extract_values("Monthly FTSE Data - New.xlsx", companies, end_date, 20)
-            model, _ = train_xgboost_model(curr_values, fut_values, model=model)
+            curr_values, fut_values, _ = extract_values("Monthly FTSE Data - New.xlsx", companies, end_date, 20)
+            if i < 3:
+                model, _ = train_xgboost_model(curr_values, fut_values, model=model)
+            else:
+                _,_ = train_xgboost_model(curr_values, fut_values, model=model)
             print(i)
             
-        fut_averages = np.mean(fut_values, axis=0) 
+        fut_averages = np.mean(fut_values, axis=0)
